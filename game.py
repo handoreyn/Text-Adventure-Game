5e7bd67a2f7fc4ba00c53d47dd29d41a2c8f8b49
handoreyn <fatihgencaslan@yahoo.com>
2019-06-02 14:57:13 +0300

    Weapon base class has been created and specific classes inherited from it.

--- game.py
+++ game.py
@@ -1,10 +1,53 @@
+class Rock:
+    def __init__(self):
+        self.name = 'Rock'
+        self.description = 'A fist-sized rock, suitable for bludgeoning.'
+        self.damage = 5
+
+    def __str__(self):
+        return self.name
+
+
+class Dagger:
+    def __init__(self):
+        self.name = 'Dagger'
+        self.description = 'A small dagger with some rust.' \
+            'Somewhat more dangerous than a rock.'
+        self.damage = 10
+
+    def __str__(self):
+        return self.name
+
+
+class RustySword:
+    def __init__(self):
+        self.name = 'Rusty Sword'
+        self.description = 'This sword is showing its age, ' \
+            'but still has some fight in it.'
+        self.damage = 20
+
+    def __str__(self):
+        return self.name
+
+
+class LightSaber:
+    def __init__(self):
+        self.name = 'Lightsaber'
+        self.description = 'Jedi''s weapon'
+        self.damage = 100
+
+    def __str__(self):
+        self.name
+
 # function for getting user's action
+
+
 def get_player_command():
     return input('Action: ')
 
 
 def play():
-    inventory = ['Dagger', 'Gold(5)', 'Crusty Bread']
+    inventory = [LightSaber(), 'Gold(5)', 'Crusty Bread']
 
     print('Escape from Cave Terror!')
 
