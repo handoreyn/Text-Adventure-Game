713970117950ce2969bc9cea75d86188dd9d403b
handoreyn <fatihgencaslan@yahoo.com>
2019-06-02 13:46:06 +0300

    game loop has been created.

--- game.py
+++ game.py
@@ -2,23 +2,29 @@
 def get_player_command():
     return input('Action: ')
 
+
 def play():
     inventory = ['Dagger', 'Gold(5)', 'Crusty Bread']
 
     print('Escape from Cave Terror!')
-    action_input = get_player_command()
 
-    if action_input == 'n' or action_input == 'N':
-        print('Go North!')
-    elif action_input == 'w' or action_input == 'W':
-        print('Go West!')
-    elif action_input == 's' or action_input == 'S':
-        print('Go South!')
-    elif action_input == 'e' or action_input == 'E':
-        print('Go East!')
-    else:
-        print('Invalid action')
+    while True:
+        action_input = get_player_command()
+
+        if action_input in ['n', 'N']:
+            print('Go North!')
+        elif action_input in ['w', 'W']:
+            print('Go West!')
+        elif action_input in ['s', 'S']:
+            print('Go South!')
+        elif action_input in ['e', 'E']:
+            print('Go East!')
+        elif action_input in ['i', 'I']:
+            print('Inventory: ')
+            for item in inventory:
+                print('*' + str(item))
+        else:
+            print('Invalid action')
 
 
 play()
- 
