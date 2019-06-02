0687789c7fa85a51f510846c21f7e4a51e9b8115
handoreyn <fatihgencaslan@yahoo.com>
2019-06-02 15:02:19 +0300

    Shit, fuck. forget to save file

--- game.py
+++ game.py
@@ -1,43 +1,35 @@
-class Rock:
+class Weapon:
+    def __str__(self):
+        return self.name
+
+class Rock(Weapon):
     def __init__(self):
         self.name = 'Rock'
         self.description = 'A fist-sized rock, suitable for bludgeoning.'
         self.damage = 5
 
-    def __str__(self):
-        return self.name
 
-
-class Dagger:
+class Dagger(Weapon):
     def __init__(self):
         self.name = 'Dagger'
         self.description = 'A small dagger with some rust.' \
             'Somewhat more dangerous than a rock.'
         self.damage = 10
 
-    def __str__(self):
-        return self.name
 
-
-class RustySword:
+class RustySword(Weapon):
     def __init__(self):
         self.name = 'Rusty Sword'
         self.description = 'This sword is showing its age, ' \
             'but still has some fight in it.'
         self.damage = 20
 
-    def __str__(self):
-        return self.name
 
-
-class LightSaber:
+class LightSaber(Weapon):
     def __init__(self):
         self.name = 'Lightsaber'
         self.description = 'Jedi''s weapon'
         self.damage = 100
-
-    def __str__(self):
-        self.name
 
 # function for getting user's action
 
