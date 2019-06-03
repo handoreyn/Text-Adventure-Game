1324e71427fbf98392d49dfd2371b2ea7c1f49f0
handoreyn <fatihgencaslan@yahoo.com>
2019-06-03 16:09:10 +0300

    class reorganized

--- items.py
+++ items.py
@@ -12,11 +12,13 @@ class Weapon:
     def __str__(self):
         return self.name
 
+
 class Rock(Weapon):
     def __init__(self):
         self.name = 'Rock'
         self.descripiton = 'A fist-sized rock, suitable for bludgeoning.'
         self.damage = 5
+
 
 class Dagger(Weapon):
     def __init__(self):
@@ -24,13 +26,15 @@ class Dagger(Weapon):
         self.descripiton = 'A small dagger with some rust. Somewhat more' \
             ' dangerous than a rock.'
         self.damage = 10
-    
+
+
 class RustySword(Weapon):
     def __init__(self):
         self.name = 'Rusty Sword'
         self.descripiton = 'This sword is showing its age, but still ' \
             'has some fight in it.'
         self.damage = 20
+
 
 class LightSaber(Weapon):
     def __init__(self):
@@ -39,6 +43,3 @@ class LightSaber(Weapon):
             'It ''s hard to find, hard to use. You should have to ' \
             'trained to use that. So, are you?'
         self.damage = 100
-        
-    
-        
