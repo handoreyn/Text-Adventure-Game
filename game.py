e8a1c0d9c28e0e101a4c204bdee6e44d76c17401
handoreyn <fatihgencaslan@yahoo.com>
2019-06-03 13:51:33 +0300

    Ready to being interesting

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 from player import Player
+import world
 
 
 def play():
@@ -8,22 +9,27 @@ def play():
     player = Player()
 
     while True:
+        room = world.tile_at(player.x, player.y)
+        print(room.intro_text())
+
         action_input = get_player_command()
 
         if action_input in ['n', 'N']:
-            print('Go North!')
+            player.move_north()
         elif action_input in ['w', 'W']:
-            print('Go West!')
+            player.move_west()
         elif action_input in ['s', 'S']:
-            print('Go South!')
+            player.move_south()
         elif action_input in ['e', 'E']:
-            print('Go East!')
+            player.move_east()
         elif action_input in ['i', 'I']:
             player.print_inventory()
         else:
             print('Invalid action')
 
+
 def get_player_command():
     return input('Action: ')
+
 
 play()
